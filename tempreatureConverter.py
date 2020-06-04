1e8d9bbb649b6f42b2627094d31707f1bfd36c4e
RaazKH <serxtdweia9r@gmail.com>
2020-06-03 22:19:17 -0500

    used a function

--- tempreatureConverter.py
+++ tempreatureConverter.py
@@ -1,2 +1,20 @@
-print('hello')
+def parseRep(userIN):
+    userIN = userIN.upper()
+    if 'Y' in userIN:
+        return True
+    return False
 
+print('Welcome to the temperature converter!')
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
+repeat = True
+loops = 0
+while repeat:
+    print ('\n')
+    print ('enter the temp code')
+    #if loops == 0:
+
+    print ('Would you like to make another calculation?')
+    repeat = parseRep(input())
+    loops += 1
+
+print('Goodbye')
