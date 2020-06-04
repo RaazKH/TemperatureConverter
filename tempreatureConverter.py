16ebf9d74273aa8bd15488bba8e60c58cc2a0220
RaazKH <serxtdweia9r@gmail.com>
2020-06-03 23:49:41 -0500

    pivoting design after this commit

--- tempreatureConverter.py
+++ tempreatureConverter.py
@@ -1,22 +1,48 @@
 print('Welcome to the temperature converter!')
 print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
+print('Conversion input example:
+    \n\"cf\" = Celsius to Fahrenheit
+    \n\"kf\" = Kelvin to Fahrenheit')
+
 repeat = True
 loops = 0
 
 while repeat:
     print('\n')
-    code = input('Enter the code:')
+    code = input('Conversion:').tolower()
     
+
+    if   ('cf' in code):
+        partA = '\N{DEGREE SIGN}C'
+        partB = '\N{DEGREE SIGN}F'
+    elif ('ck' in code):
+        partA = '\N{DEGREE SIGN}C'
+        partB = 'K'
+    elif ('kf' in code):
+        partA = 'K'
+        partB = '\N{DEGREE SIGN}F'
+    elif ('kc' in code):
+        partA = 'K'
+        partB = '\N{DEGREE SIGN}C'
+    elif ('fc' in code):
+        partA = 'K'
+        partB = '\N{DEGREE SIGN}F'
+    elif ('fk' in code):
+        partA = 'K'
+        partB = '\N{DEGREE SIGN}C'
+        
     valid = False
     while valid != True:
         try:
-            temp= float(input('Enter the temperature in \N{DEGREE SIGN}' + 'C' + ':'))
+            temp= float(input('Enter the temperature in ' + partA + ':'))
             valid = True
         except ValueError:
-            print('\nPlease enter a valid temperature!\n')
+            print('\nInvalid temperature!!!\n')
 
+    
     print ('Would you like to make another calculation?')
     repeat = 'Y' in input().upper()
     loops += 1
 
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
 print('\nGoodbye')
