18ae27b6d7495889ff74e11ee43854918ccc1239
RaazKH <serxtdweia9r@gmail.com>
2020-06-04 01:00:13 -0500

    mvp finished

--- tempreatureConverter.py
+++ tempreatureConverter.py
@@ -1,48 +1,59 @@
+def fahrenheit(temp):
+    print('%.2f\N{DEGREE SIGN}F' % temp)
+    temp = (temp - 32) * (5.0 / 9.0)
+    print('%.2f\N{DEGREE SIGN}C' % temp)
+    print('%.2fK' % (temp + 273.15))
+    return
+
+def celsius(temp):
+    print('%.2f\N{DEGREE SIGN}C' % temp)
+    print('%.2f\N{DEGREE SIGN}F' % (temp * ((5.0 / 9.0)) + 32))
+    print('%.2fK' % (temp + 273.15))
+    return
+
+def kelvin(temp):
+    print('%.2fK' % temp)
+    temp -= 273.15
+    print('%.2f\N{DEGREE SIGN}C' % temp)
+    print('%.2f\N{DEGREE SIGN}F' % (temp * ((5.0 / 9.0)) + 32))
+    return
+
 print('Welcome to the temperature converter!')
-print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
-print('Conversion input example:
-    \n\"cf\" = Celsius to Fahrenheit
-    \n\"kf\" = Kelvin to Fahrenheit')
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
 repeat = True
-loops = 0
-
-while repeat:
-    print('\n')
-    code = input('Conversion:').tolower()
-    
-
-    if   ('cf' in code):
-        partA = '\N{DEGREE SIGN}C'
-        partB = '\N{DEGREE SIGN}F'
-    elif ('ck' in code):
-        partA = '\N{DEGREE SIGN}C'
-        partB = 'K'
-    elif ('kf' in code):
-        partA = 'K'
-        partB = '\N{DEGREE SIGN}F'
-    elif ('kc' in code):
-        partA = 'K'
-        partB = '\N{DEGREE SIGN}C'
-    elif ('fc' in code):
-        partA = 'K'
-        partB = '\N{DEGREE SIGN}F'
-    elif ('fk' in code):
-        partA = 'K'
-        partB = '\N{DEGREE SIGN}C'
+while repeat:    
+    valid = False
+    while valid != True:
+        valid = True
+        code = input('\nConvert from: ').lower()
+        if   ('c' in code):
+            code = '\N{DEGREE SIGN}C'
+        elif ('k' in code):
+            code = 'K'
+        elif ('f' in code):
+            code = '\N{DEGREE SIGN}F'
+        else:
+            print('\nInvalid input!\n')
+            valid = False
         
     valid = False
     while valid != True:
         try:
-            temp= float(input('Enter the temperature in ' + partA + ':'))
+            temp= float(input('Enter the temperature in ' + code + ': '))
             valid = True
         except ValueError:
             print('\nInvalid temperature!!!\n')
-
+            
+    print()
+    if  ('C' in code):
+        celsius(temp)
+    elif('F' in code):
+        fahrenheit(temp)
+    else:
+        kelvin(temp)
     
-    print ('Would you like to make another calculation?')
-    repeat = 'Y' in input().upper()
-    loops += 1
-
-print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
-print('\nGoodbye')
+    repeat = 'Y' in input('\nWould you like to make another calculation?\n').upper()
+    print('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    
+print('               Goodbye')
